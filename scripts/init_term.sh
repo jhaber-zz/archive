ad9b4addfafcb2d357b6b63440b0721db5754de1
Jaren Haber <jhaber@js-168-207.jetstream-cloud.org>
2018-07-10 19:47:41 -0400

    added decription and shapely

--- scripts/init_term.sh
+++ scripts/init_term.sh
@@ -1,7 +1,15 @@
+# Simple script to install additional packages to Jetstream VM using Ubuntu 16.04 image
+
+# Author: Jaren Haber
+# First created: Spring 2018
+# Last updated: Summer 2018
+
 pip install --upgrade pip
 pip install --upgrade pandas
 pip install tqdm
 pip install nltk
-pip install tqdm
 pip install timeout-decorator
 pip install lxml
+pip install boxsdk
+pip install geopandas
+pip install shapely
